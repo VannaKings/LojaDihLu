0ecf2d3eb9afc75acf3f0a35113d36b128645127
Vanna <vanna.gege@gmail.com>
2022-05-27 15:33:24 -0300

    Adição na página do index e troca de cores

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -53,6 +53,42 @@ $('.destaque').slick({
         }
       },
       {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.avaliacao').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    prevArrow: $("#arrow-prev2"),
+    nextArrow: $("#arrow-next2"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      },
+      {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
