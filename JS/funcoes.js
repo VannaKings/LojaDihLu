bb085cb55fa34f555ba900ed29fb9e661e0c125b
Vanna <vanna.gege@gmail.com>
2022-06-04 19:41:02 -0300

    alteração na responsividade

--- JS/funcoes.js
+++ JS/funcoes.js
@@ -45,7 +45,7 @@ $('.destaque').slick({
     nextArrow: $("#arrow-next1"),
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1100,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
@@ -55,43 +55,7 @@ $('.destaque').slick({
       {
         breakpoint: 800,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-  $('.avaliacao').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    prevArrow: $("#arrow-prev2"),
-    nextArrow: $("#arrow-next2"),
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
           slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
           slidesToScroll: 1
         }
       },
